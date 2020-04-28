e5eb3fb19e2559a70bfae4a6138f2b213929f0ed
priyamvad <priyamvad123@gmail.com>
2020-04-28 22:42:14 +0530

    updated excel sheet

---
+++ Pizza-delievry-system/src/main/resources/schema_pos_final.sql
@@ -0,0 +1,89 @@
+﻿CREATE TABLE POS_TBL_User_Profile (
+UserId  VARCHAR(6) NOT NULL,
+Firstname VARCHAR(20) NOT NULL,
+Lastname VARCHAR(20) NOT NULL,
+DateOfBirth DATE NOT NULL,
+Gender VARCHAR(7) NOT NULL,
+Street VARCHAR(30) NOT NULL,
+Location VARCHAR(20) NOT NULL,
+City VARCHAR(20) NOT NULL,
+User_State VARCHAR(20) NOT NULL,
+Pincode VARCHAR(10),
+MobileNo INT,
+EmailId VARCHAR(30) NOT NULL,
+PRIMARY KEY (UserId)
+);
+
+CREATE TABLE POS_TBL_User_Credentials (
+UserId  VARCHAR(6) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+UserType VARCHAR(15),
+LoginStatus INT DEFAULT NULL,
+PRIMARY KEY (UserId),
+CONSTRAINT POS_TBL_User_Credentials_check_1 CHECK ((UserType in (_utf8mb4 'A',_utf8mb4'C'))),
+CONSTRAINT POS_TBL_User_Credentials_check_2 CHECK ((LoginStatus in (0,1)))
+);
+
+CREATE TABLE POS_TBL_Pizza_Store(
+StoreId VARCHAR(6) NOT NULL,
+Name VARCHAR(15) NOT NULL,
+Street VARCHAR(50) NOT NULL,
+MobileNo INT NOT NULL,
+City VARCHAR(15) NOT NULL,
+Store_State VARCHAR(15) NOT NULL,
+Pincode VARCHAR(10),
+PRIMARY KEY(StoreId)
+);
+
+CREATE TABLE POS_TBL_Food(
+FoodId VARCHAR(6) NOT NULL,
+Name VARCHAR(20) NOT NULL,
+FoodType VARCHAR(10) NOT NULL,
+FoodSize VARCHAR(15) NOT NULL,
+Quantity INT NOT NULL,
+Price INT NOT NULL,
+PRIMARY KEY (FoodId),
+CONSTRAINT POS_TBL_Food_check_1 CHECK ((FoodType in (Veg,Non-Veg))),
+CONSTRAINT POS_TBL_Food_check_2 CHECK ((FoodSize in (Small,Medium,Big)))
+);
+
+CREATE TABLE POS_TBL_Order(
+OrderId VARCHAR(6) NOT NULL,
+UserId VARCHAR(6) NOT NULL,
+OrderDate DATE NOT NULL,
+StoreId VARCHAR(6) NOT NULL,
+TotalPrice INT NOT NULL,
+OrderStatus VARCHAR(15) NOT NULL,
+CartId INT,
+Street VARCHAR(50) NOT NULL,
+City VARCHAR(15) NOT NULL,
+Order_State VARCHAR(15) NOT NULL,
+Pincode VARCHAR(10),
+MobileNo VARCHAR(10),
+PRIMARY KEY (OrderId),
+FOREIGN KEY (UserId) REFERENCES POS_TBL_User_Profile(UserId),
+FOREIGN KEY (StoreId) REFERENCES POS_TBL_Pizza_Store(StoreId),
+CONSTRAINT POS_TBL_Order_check_1 CHECK ((OrderStatus in (Confirmed,Delivered,Pending,Cancelled)))
+);
+
+CREATE TABLE POS_TBL_Cart(
+CartId INT,
+UserId VARCHAR(6) NOT NULL,
+FoodId VARCHAR(6) NOT NULL,
+FoodType VARCHAR(10) NOT NULL,
+Quantity INT NOT NULL,
+Cost INT NOT NULL,
+OrderDate DATE,
+FOREIGN KEY (UserId) REFERENCES POS_TBL_User_Profile(UserId),
+FOREIGN KEY (FoodId) REFERENCES POS_TBL_Food(FoodId),
+CONSTRAINT POS_TBL_Food_check_1 CHECK ((FoodType in (Veg,Non-Veg)))
+);
+
+CREATE TABLE POS_TBL_CreditCard(
+CreditCardNumber VARCHAR(16) NOT NULL,
+ValidFrom VARCHAR(7) NOT NULL,
+ValidTo VARCHAR(7) NOT NULL,
+Balance INT NOT NULL,
+userId VARCHAR(6),
+PRIMARY KEY (CreditCardNumber)
+);
