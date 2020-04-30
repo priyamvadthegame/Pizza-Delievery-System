f9bbaba727d9458bab1b67f92b573cd44dcf0190
Deeptansu <deeptansu99@gmail.com>
2020-05-01 00:16:39 +0530

    Food Controller,service completed

--- Pizza-delievry-system/src/main/java/com/project/services/FoodServiceImpl.java
+++ Pizza-delievry-system/src/main/java/com/project/services/FoodServiceImpl.java
@@ -1,5 +1,41 @@
 package com.project.services;
 
-public class FoodServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.json.Food;
+import com.project.repositories.FoodRepository;
+
+@Service
+public class FoodServiceImpl implements FoodService {
+
+	@Autowired
+	private FoodRepository foodRepository;
+
+	@Override
+	public boolean delete(String id) {
+		
+		return false;
+	}
+
+	@Override
+	public Food save(Food food) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Food update(Food food, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Food> getAllFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
