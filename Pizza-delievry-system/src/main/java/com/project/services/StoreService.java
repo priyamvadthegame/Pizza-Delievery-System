48e8804f29e3ee18d07b0cd6482d5f8c4ea5e310
ShubhamDarkEvil <60914719+ShubhamDarkEvil@users.noreply.github.com>
2020-05-01 14:31:50 +0530

    Update StoreService.java

--- Pizza-delievry-system/src/main/java/com/project/services/StoreService.java
+++ Pizza-delievry-system/src/main/java/com/project/services/StoreService.java
@@ -1,5 +1,17 @@
 package com.project.services;
 
+import java.util.List;
+
+import com.project.json.Food;
+import com.project.json.Store;
+
 public interface StoreService {
+	public Store registerStore(Store store);
+	public boolean deleteStore(Long id);
+	public List<Store> filterStoreByFood(Long id);
+	public List<Food> getFoodInStore(Long id);
+	public Store save(Store store);
+	
+	
 
 }
