1f9e4b01a6567b52239274c1c1290f3fe1322196
priyamvad <priyamvad123@gmail.com>
2020-05-01 14:38:45 +0530

    added store utils by subham

--- Pizza-delievry-system/src/main/java/com/project/utils/StoreUtils.java
+++ Pizza-delievry-system/src/main/java/com/project/utils/StoreUtils.java
@@ -3,28 +3,26 @@ package com.project.utils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
-import com.project.entity.FoodEntity;
 import com.project.entity.StoreEntity;
-import com.project.json.Food;
-import com.project.json.Store;
+
+import com.project.json.StoreJson;
 
 public class StoreUtils {
 
-	public static List<Store> convertStoreEntityListToStoreJson(List<StoreEntity> storeEntityList)
+	public static List<StoreJson> convertStoreEntityListToStoreJson(List<StoreEntity> storeEntityList)
 	{
-		List<Store> stList=new ArrayList<Store>();
+		List<StoreJson> stList=new ArrayList<StoreJson>();
 		Consumer<StoreEntity> consumer=(StoreEntity st)->stList.add(convertStoreEntityToStoreJson(st));
 		storeEntityList.stream().forEach(consumer);
 		return stList;
 	}
 	
-	public static Store convertStoreEntityToStoreJson(StoreEntity storeEntity)
+	public static StoreJson convertStoreEntityToStoreJson(StoreEntity storeEntity)
 	{
-		return new Store(storeEntity.getStoreName(),storeEntity.getStoreStreet(),storeEntity.getStoreMobileNo(),storeEntity.getStoreCity(),storeEntity.getStoreState(),storeEntity.getStorePincode());
+		return new StoreJson(storeEntity.getStoreName(),storeEntity.getStoreStreet(),storeEntity.getStoreMobileNo(),storeEntity.getStoreCity(),storeEntity.getStoreState(),storeEntity.getStorePincode());
 	}
 	
-	public static StoreEntity convertStoreJsonToStoreEntity(Store store)
+	public static StoreEntity convertStoreJsonToStoreEntity(StoreJson store)
 	{
 		return new StoreEntity(store.getStoreName(),store.getStoreStreet(),store.getStoreMobileNo(),store.getStoreCity(),store.getStoreState(),store.getStorePincode());
 	}
